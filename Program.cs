dcf9adb08847871b165d0358aaa10dcfb5a21982
SurveyZerg <50334084+SurveyZerg@users.noreply.github.com>
2019-09-25 18:15:08 +0200

    Creating

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dungeon_Starcraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
